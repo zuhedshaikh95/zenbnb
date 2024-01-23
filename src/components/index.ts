234d3747912657bda24686a947cf0d39dab45ad5
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-23 23:50:16 +0530

    add: main exports

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// Navbar
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Logo } from "./Navbar/Logo";
+export { default as Search } from "./Navbar/Search";
+export { default as UserMenu } from "./Navbar/UserMenu";
+
+export { default as Container } from "./Container";
+export { default as Avatar } from "./Avatar";
