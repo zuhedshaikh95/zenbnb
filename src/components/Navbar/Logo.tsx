8c467cc7c6c91e26bc9eb63860256df2b38a6a2b
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-23 23:50:53 +0530

    add: Logo

---
+++ src/components/Navbar/Logo.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo: React.FC = () => {
+  const router = useRouter();
+
+  return <Image className="hidden md:block cursor-pointer" src="/media/logo.png" height="100" width="100" alt="logo" />;
+};
+
+export default Logo;
