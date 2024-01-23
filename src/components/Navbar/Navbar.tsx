c45ad9a4a4944e35b41ac470737c07d9a5e2c7a0
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-23 23:51:19 +0530

    add: Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { Container, Logo, Search, UserMenu } from "..";
+
+interface Props {}
+
+const Navbar: React.FC<Props> = ({}) => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
