fac0cd8030179c7ab96a91870850f24a9ed298a9
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-30 23:09:18 +0530

    add: menu

--- src/components/Navbar/UserMenu.tsx
+++ src/components/Navbar/UserMenu.tsx
@@ -1,24 +1,35 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import { Avatar } from "..";
+import { HiOutlineGlobeAlt } from "react-icons/hi";
+import { Avatar, MenuItem } from "..";
 
 interface Props {}
 
 const UserMenu: React.FC<Props> = ({}) => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const handleToggleShowMenu = () => setShowMenu((prev) => !prev);
+
   return (
     <div className="relative">
-      <div className="flex items-center fap-3">
-        <div
-          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-          onClick={() => {}}
-        >
-          Zenbnb your home
+      <div className="flex items-center gap-3">
+        <div className="flex items-center">
+          <div
+            className="hidden md:block text-sm font-semibold py-3  px-4 rounded-full hover:bg-neutral-200 transition cursor-pointer"
+            onClick={() => {}}
+          >
+            Zenbnb your home
+          </div>
+
+          <div className="hidden md:block hover:bg-neutral-200 cursor-pointer rounded-full py-3 px-4 text-[#222]">
+            <HiOutlineGlobeAlt size={18} />
+          </div>
         </div>
 
         <div
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-          onClick={() => {}}
+          onClick={() => handleToggleShowMenu()}
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
@@ -26,6 +37,15 @@ const UserMenu: React.FC<Props> = ({}) => {
           </div>
         </div>
       </div>
+
+      {showMenu && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-11/12 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <MenuItem label="Sign up" onClick={() => {}} />
+            <MenuItem label="Log in" onClick={() => {}} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
