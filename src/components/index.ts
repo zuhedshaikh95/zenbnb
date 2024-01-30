91225d2b54c2a189d3c9dcbfc91b00ebc1dfd139
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-30 22:44:22 +0530

    add: export MenuItem

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Logo } from "./Navbar/Logo";
 export { default as Search } from "./Navbar/Search";
 export { default as UserMenu } from "./Navbar/UserMenu";
+export { default as MenuItem } from "./Navbar/MenuItem";
 
 export { default as Container } from "./Container";
 export { default as Avatar } from "./Avatar";
