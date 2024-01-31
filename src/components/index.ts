b750d7b7dc386e13245bad5b3fd6daf6ff9e59f3
zuhedshaikh95 <zuhed95@gmail.com>
2024-01-31 23:30:13 +0530

    add: export Button

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,5 +5,9 @@ export { default as Search } from "./Navbar/Search";
 export { default as UserMenu } from "./Navbar/UserMenu";
 export { default as MenuItem } from "./Navbar/MenuItem";
 
+// Modal
+export { default as Modal } from "./Modals/Modal";
+
 export { default as Container } from "./Container";
 export { default as Avatar } from "./Avatar";
+export { default as Button } from "./Button";
