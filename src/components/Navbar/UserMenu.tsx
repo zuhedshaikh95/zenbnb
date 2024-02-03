6fe6494ab05980e16c009273350496b45ff97943
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-03 22:58:48 +0530

    add: registerModal hook

--- src/components/Navbar/UserMenu.tsx
+++ src/components/Navbar/UserMenu.tsx
@@ -3,26 +3,28 @@ import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { HiOutlineGlobeAlt } from "react-icons/hi";
 import { Avatar, MenuItem } from "..";
+import { useRegisterModal } from "@/hooks";
 
 interface Props {}
 
 const UserMenu: React.FC<Props> = ({}) => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const registerModal = useRegisterModal();
 
   const handleToggleShowMenu = () => setShowMenu((prev) => !prev);
 
   return (
     <div className="relative">
       <div className="flex items-center gap-3">
-        <div className="flex items-center">
+        <div className="hidden md:flex md:items-center">
           <div
-            className="hidden md:block text-sm font-semibold py-3  px-4 rounded-full hover:bg-neutral-200 transition cursor-pointer"
+            className="text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-200 transition cursor-pointer"
             onClick={() => {}}
           >
             Zenbnb your home
           </div>
 
-          <div className="hidden md:block hover:bg-neutral-200 cursor-pointer rounded-full py-3 px-4 text-[#222]">
+          <div className="hover:bg-neutral-200 cursor-pointer rounded-full py-3 px-4 text-[#222]">
             <HiOutlineGlobeAlt size={18} />
           </div>
         </div>
@@ -41,7 +43,7 @@ const UserMenu: React.FC<Props> = ({}) => {
       {showMenu && (
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-11/12 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
-            <MenuItem label="Sign up" onClick={() => {}} />
+            <MenuItem label="Sign up" onClick={registerModal.onOpen} />
             <MenuItem label="Log in" onClick={() => {}} />
           </div>
         </div>
