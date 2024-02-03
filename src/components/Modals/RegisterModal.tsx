807f38ddf07f10c64f824d356b28d3a668052fce
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-03 22:57:22 +0530

    add: Modal, RegisterModal

---
+++ src/components/Modals/RegisterModal.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useCallback, useState } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useRegisterModal } from "@/hooks";
+import axios from "axios";
+import Modal from "./Modal";
+import { Heading, Input } from "..";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post("/api/register", values);
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Zenbnb" subtitle="Create an account!" />
+
+      <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModal;
