408d4bb273e6b29892f0e8efd262eda28dee7a34
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-05 22:27:01 +0530

    add: register route

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,39 @@
+"use server";
+import { prisma } from "@/libs";
+import { CustomException } from "@/utils";
+import bcrypt from "bcrypt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, name, password } = await request.json();
+
+    if (!email || !name || !password) {
+      throw new CustomException(400, "BAD REQUEST!, Invalid payload");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      { data: user, message: "New user created!", success: true },
+      {
+        status: 201,
+      }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: error.message, success: false },
+      {
+        status: error instanceof CustomException ? error.status : 500,
+      }
+    );
+  }
+}
