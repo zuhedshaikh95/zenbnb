6169bd9d0a95003adde3f7b94eaaaba585041abd
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-05 22:28:14 +0530

    add: customException

---
+++ src/utils/customException.ts
@@ -0,0 +1,10 @@
+class CustomException extends Error {
+  status: number;
+  constructor(status: number, message: string) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+}
+
+export default CustomException;
