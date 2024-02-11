2630bf00bda27b15b3cde001f97c656ff8498c11
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-11 22:32:56 +0530

    add: images remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+        protocol: "https",
+        pathname: "/u/**",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https",
+        pathname: "/a/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
