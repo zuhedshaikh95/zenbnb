d180d957283995c72688875d9a2793e542e54e88
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-12 21:35:15 +0530

    add: home route click

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -6,7 +6,16 @@ import { useRouter } from "next/navigation";
 const Logo: React.FC = () => {
   const router = useRouter();
 
-  return <Image className="hidden md:block cursor-pointer" src="/media/logo.png" height="100" width="100" alt="logo" />;
+  return (
+    <Image
+      className="hidden md:block cursor-pointer"
+      src="/media/logo.png"
+      height="100"
+      width="100"
+      alt="logo"
+      onClick={() => router.push("/")}
+    />
+  );
 };
 
 export default Logo;
