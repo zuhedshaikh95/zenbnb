f4bc7956a52b2476b26332cd7aa9ce0cbe4fda28
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-12 21:36:18 +0530

    add: CategoryBox

---
+++ src/components/CategoryBox.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback } from "react";
+import { IconType } from "react-icons";
+import qs from "query-string";
+
+interface Props {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+}
+
+const CategoryBox: React.FC<Props> = ({ icon: Icon, label, selected }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let query = {};
+
+    if (params) {
+      query = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...query,
+      category: label,
+    };
+
+    console.log(params?.get("category"));
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+
+  return (
+    <div
+      className={`
+        flex
+        flex-col
+        items-center
+        justify-center
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? "border-b-neutral-800" : "border-transparent"}
+        ${selected ? "text-neutral-800" : "text-neutral-500"}
+      `}
+      onClick={() => handleClick()}
+    >
+      <Icon size={26} />
+      <p className="font-medium text-sm">{label}</p>
+    </div>
+  );
+};
+
+export default CategoryBox;
