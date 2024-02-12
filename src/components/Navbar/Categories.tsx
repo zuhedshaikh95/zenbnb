c95666fd2134f98d962b940ac3097ea671a98af6
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-12 21:34:26 +0530

    add: Categories

---
+++ src/components/Navbar/Categories.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { categories } from "@/configs/categories.config";
+import { usePathname, useSearchParams } from "next/navigation";
+import React from "react";
+import { CategoryBox, Container } from "..";
+
+interface Props {}
+
+const Categories: React.FC<Props> = ({}) => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) return null;
+
+  return (
+    <Container>
+      <div className="pt-4 flex items-center justify-between overflow-x-auto">
+        {categories.map((item) => (
+          <CategoryBox key={item.label} selected={category === item.label} {...item} />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
