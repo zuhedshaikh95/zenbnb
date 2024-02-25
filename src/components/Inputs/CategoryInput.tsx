2c2411b85cb590767484b6bbe4a3c58d82f87a02
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-25 18:51:09 +0530

    add: CategoryInput

---
+++ src/components/Inputs/CategoryInput.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { IconType } from "react-icons";
+
+interface Props {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+  onClick: (value: string) => void;
+}
+
+const CategoryInput: React.FC<Props> = ({ icon: Icon, onClick, label, selected }) => {
+  console.log(selected, label);
+
+  return (
+    <div
+      className={`
+          rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer 
+          ${selected ? "border-black" : "border-neutral-200"}
+        `}
+      onClick={() => onClick(label)}
+    >
+      <Icon size={25} />
+      <p className="font-semibold">{label}</p>
+    </div>
+  );
+};
+
+export default CategoryInput;
