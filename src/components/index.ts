dd754a6cfa82ae72591194898bf36dd525cc3851
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-25 18:50:23 +0530

    add: CategoryInput

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,6 +13,7 @@ export { default as RentModal } from "./Modals/RentModal";
 
 // Inputs
 export { default as Input } from "./Inputs/Input";
+export { default as CategoryInput } from "./Inputs/CategoryInput";
 
 export { default as Container } from "./Container";
 export { default as Avatar } from "./Avatar";
