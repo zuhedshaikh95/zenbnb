d1f6b6287da16910c42f8058fde48602d4bb640f
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-26 22:20:49 +0530

    add: CountrySelect

--- src/components/Inputs/CountrySelect.tsx
+++ src/components/Inputs/CountrySelect.tsx
@@ -13,8 +13,36 @@ const CountrySelect: React.FC<Props> = ({ onChange, value }) => {
   const { getAllCountries } = useCountries();
 
   return (
-    <div className="">
-      <Select placeholder="Anywhere" isClearable />
+    <div>
+      <Select
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
+          colors: {
+            ...theme.colors,
+            primary: "#000",
+            primary25: "#ffe4e6",
+          },
+        })}
+        classNames={{
+          control: () => "p-1",
+          input: () => "text-md",
+          option: () => "text-md",
+        }}
+        placeholder="Anywhere"
+        isClearable
+        options={getAllCountries()}
+        value={value}
+        onChange={(value) => onChange(value as CountryI)}
+        formatOptionLabel={(option: CountryI) => (
+          <div className="flex items-center gap-3">
+            <div>{option.flag}</div>
+            <p>
+              {option.label}, <span className="text-neutral-500 ml-1">{option.region}</span>
+            </p>
+          </div>
+        )}
+      />
     </div>
   );
 };
