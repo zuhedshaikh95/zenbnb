cf4f12e17f866e3fa8be07e94456bdfe145cc5f4
zuhedshaikh95 <zuhed95@gmail.com>
2024-02-28 09:17:27 +0530

    add: LOCATION, IMAGE step

--- src/components/Modals/RentModal.tsx
+++ src/components/Modals/RentModal.tsx
@@ -4,8 +4,9 @@ import { useRentModal } from "@/hooks";
 import { CountryI, RentModalStepsE } from "@/types";
 import React, { useMemo, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { CategoryInput, CountrySelect, Heading, Map } from "..";
+import { CategoryInput, Counter, CountrySelect, Heading, ImageUploader } from "..";
 import Modal from "./Modal";
+import dynamic from "next/dynamic";
 
 interface Props {}
 
@@ -27,6 +28,7 @@ const RentModal: React.FC<Props> = ({}) => {
       location: null,
       guestCount: 1,
       bathroomCount: 1,
+      roomCount: 1,
       imageSrc: "",
       price: 1,
       title: "",
@@ -36,6 +38,12 @@ const RentModal: React.FC<Props> = ({}) => {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const bathroomCount = watch("bathroomCount");
+  const roomCount = watch("roomCount");
+  const imageSrc = watch("imageSrc");
+
+  const Map = useMemo(() => dynamic(() => import("../Map")), [location]);
 
   const setCustomValue = (id: string, value: string | number | null | CountryI) => {
     setValue(id, value, {
@@ -69,11 +77,11 @@ const RentModal: React.FC<Props> = ({}) => {
     return "Back";
   }, [step]);
 
-  let bodyContent = useMemo(() => {
+  const bodyContent = (() => {
     switch (step) {
       case RentModalStepsE.CATEGORY:
         return (
-          <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
             <Heading title="Which of these best describes your place?" subtitle="Pick a category" />
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-auto">
@@ -93,19 +101,55 @@ const RentModal: React.FC<Props> = ({}) => {
 
       case RentModalStepsE.LOCATION:
         return (
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 h-full">
             <Heading title="Where is your place located?" subtitle="Help guests find you!" />
 
             <CountrySelect onChange={(value) => setCustomValue("location", value)} value={location} />
 
-            <Map />
+            <Map center={location?.latlng} />
           </div>
         );
 
+      case RentModalStepsE.INFO:
+        return (
+          <div className="flex flex-col gap-4">
+            <Heading title="Share some basics about your place" subtitle="What amenities do you have?" />
+
+            <Counter
+              title="Guests"
+              subtitle="How many guests do you allow?"
+              onChange={(value) => setCustomValue("guestCount", value)}
+              value={guestCount}
+            />
+            <hr />
+            <Counter
+              title="Rooms"
+              subtitle="How many rooms do you have?"
+              onChange={(value) => setCustomValue("roomCount", value)}
+              value={roomCount}
+            />
+            <hr />
+            <Counter
+              title="Bathrooms"
+              subtitle="How many bathrooms do you have?"
+              onChange={(value) => setCustomValue("bathroomCount", value)}
+              value={bathroomCount}
+            />
+          </div>
+        );
+
+      case RentModalStepsE.IMAGES:
+        return (
+          <div className="flex flex-col gap-4">
+            <Heading title="Add a photo of your place" subtitle="Show guests wht your place looks like!" />
+
+            <ImageUploader onChange={(url) => setCustomValue("imageSrc", url)} value={imageSrc} />
+          </div>
+        );
       default:
         <div></div>;
     }
-  }, [category, location, step]);
+  })();
 
   return (
     <Modal
