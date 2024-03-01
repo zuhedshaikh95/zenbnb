a4294a488ced13973d37870e4bce12bb034f2a20
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-01 21:39:24 +0530

    fix: xl screen padding

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const Container: React.FC<Props> = ({ children }) => {
-  return <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">{children}</div>;
+  return <div className="max-w-[2520px] mx-auto md:px-10 sm:px-2 px-4">{children}</div>;
 };
 
 export default Container;
