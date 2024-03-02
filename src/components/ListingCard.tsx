986bd6a83144d24791ee7d0c986dd2e66c9e2235
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-02 23:59:13 +0530

    add: price, reservation, date, hover effect

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -1,17 +1,73 @@
+"use client";
+import { useCountries } from "@/hooks";
 import { Listing, Reservation, User } from "@prisma/client";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useCallback, useMemo } from "react";
+import { format } from "date-fns";
+import Image from "next/image";
 
 interface Props {
   data?: Listing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
+  disabled?: boolean;
   actionLabel?: string;
   actionId?: string;
   user?: User | null;
 }
 
-const ListingCard: React.FC<Props> = ({ actionId, actionLabel, data, onAction, reservation, user }) => {
-  return <div>ListingCard</div>;
+const ListingCard: React.FC<Props> = ({ actionId = "", actionLabel, data, onAction, reservation, user, disabled }) => {
+  const router = useRouter();
+  const { getCountryByValue } = useCountries();
+
+  const location = getCountryByValue(data?.locationValue!);
+
+  const handleCancel = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+
+      onAction!(actionId);
+    },
+    [onAction, actionId, disabled]
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data?.price;
+  }, [reservation, data?.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div className="col-span-1 cursor-pointer group" onClick={() => router.push(`/listings/${data?.id}`)}>
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            className="object-cover h-full w-full group-hover:scale-110 transition"
+            src={data?.imageSrc!}
+            alt="listing"
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
