3029c6faa3d370755e22a7bf8123905e1e728015
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-03 19:12:52 +0530

    fix: default center

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -23,9 +23,9 @@ const Map: React.FC<Props> = ({ center }) => {
   return (
     <MapContainer
       center={center || [51, -0.09]}
-      zoom={center ? 8 : 2}
+      zoom={center ? 4 : 2}
       scrollWheelZoom={false}
-      className="h-full md:h-[40vh] rounded-lg"
+      className="h-full min-h-[40vh] rounded-lg"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
