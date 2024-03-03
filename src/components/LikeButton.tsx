0569b3d8668371a1ead5c8d08e9bb70c75e89d0d
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-03 16:40:17 +0530

    add: LikeButton

---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,23 @@
+import { useFavorite } from "@/hooks";
+import { User } from "@prisma/client";
+import React from "react";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+
+interface Props {
+  listingId: string;
+  user?: User | null;
+}
+
+const LikeButton: React.FC<Props> = ({ listingId, user }) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({ listingId, user });
+
+  return (
+    <div className="relative hover:opacity-100 transition cursor-pointer" onClick={toggleFavorite}>
+      <AiOutlineHeart className="fill-white absolute" size={25} />
+
+      <AiFillHeart className={`${hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}`} size={25} />
+    </div>
+  );
+};
+
+export default LikeButton;
