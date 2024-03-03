e76336db5b1c83737dead607e13a806e2378eeae
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-03 19:13:59 +0530

    add: ListingHead

---
+++ src/components/Listings/ListingHead.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useCountries } from "@/hooks";
+import { User } from "@prisma/client";
+import React from "react";
+import { Heading, LikeButton } from "..";
+import Image from "next/image";
+
+interface Props {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  user?: User;
+}
+
+const ListingHead: React.FC<Props> = ({ id, locationValue, title, imageSrc, user }) => {
+  const { getCountryByValue } = useCountries();
+  const location = getCountryByValue(locationValue);
+
+  return (
+    <>
+      <Heading title={title} subtitle={`${location?.region}, ${location?.label}`} />
+
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image className="object-cover w-full" src={imageSrc} fill alt="listing" />
+
+        <div className="absolute top-5 right-5">
+          <LikeButton listingId={id} user={user} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListingHead;
