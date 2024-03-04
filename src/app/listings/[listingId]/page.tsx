e8183435b38920ad8a3cf73b8fc15b4c8d57b5e1
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-04 22:27:30 +0530

    add: dynamic listing

--- src/app/listings/[listingId]/page.tsx
+++ src/app/listings/[listingId]/page.tsx
@@ -1,43 +1,19 @@
 import getListingById from "@/actions/getListingById";
-import { Container, EmptyState, ListingHead, ListingInfo } from "@/components";
-import { categories } from "@/configs/categories.config";
+import { EmptyState } from "@/components";
+import ListingClient from "./ListingClient";
+import { getReservations } from "@/actions";
 
 interface ParamsI {
   listingId?: string;
 }
 
-export default async function ListingPage({ params }: { params: ParamsI }) {
+export default async function Listing({ params }: { params: ParamsI }) {
   const listing = await getListingById(params);
-  const category = categories.find((category) => category.label === listing?.category);
+  const reservations = await getReservations(params);
 
   if (!listing) {
     return <EmptyState />;
   }
 
-  return (
-    <Container>
-      <div className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-6">
-          <ListingHead
-            title={listing?.title}
-            imageSrc={listing?.imageSrc}
-            locationValue={listing?.locationValue}
-            id={listing?.id}
-            user={listing?.user}
-          />
-          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
-            <ListingInfo
-              user={listing?.user}
-              category={category}
-              description={listing.description}
-              roomCount={listing.roomCount}
-              guestCount={listing.guestCount}
-              bathroomCount={listing.bathroomCount}
-              locationValue={listing.locationValue}
-            />
-          </div>
-        </div>
-      </div>
-    </Container>
-  );
+  return <ListingClient listing={listing} user={listing.user} reservations={reservations!} />;
 }
