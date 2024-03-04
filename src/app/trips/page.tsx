e6b31fee26f92f84a8c6867105a8fa8c3bf9ba80
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-04 22:28:05 +0530

    add: all trips

---
+++ src/app/trips/page.tsx
@@ -0,0 +1,23 @@
+import { EmptyState } from "@/components";
+import getUser from "@/actions/getUser";
+import { getReservations } from "@/actions";
+import TripsClient from "./TripsClient";
+
+export default async function Trips() {
+  const user = await getUser();
+  const reservations = await getReservations({ userId: user?.id });
+
+  if (!user) {
+    return <EmptyState title="Unauthorized!" subtitle="Please login" />;
+  }
+
+  if (reservations.length === 0) {
+    return <EmptyState title="No trips found!" subtitle="Looks like you haven't reserved any trips" />;
+  }
+
+  return (
+    <div>
+      <TripsClient reservations={reservations} user={user} />
+    </div>
+  );
+}
