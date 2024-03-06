e0434a102d15703cf52b120223feb8db733be604
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-06 21:52:14 +0530

    add: my reservations page

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -1,3 +1,24 @@
+import { EmptyState } from "@/components";
+import getUser from "@/actions/getUser";
+import { getReservations } from "@/actions";
+import ReservationsClient from "./ReservationsClient";
+
 export default async function Reservations() {
-  return <div>Lassan</div>;
+  const user = await getUser();
+
+  if (!user) {
+    return <EmptyState title="Unauthorized" subtitle="Please login!" />;
+  }
+
+  const reservations = await getReservations({
+    authorId: user?.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <EmptyState title="No reservations found!" subtitle="Looks like you have no reservations on your property" />
+    );
+  }
+
+  return <ReservationsClient reservations={reservations} user={user} />;
 }
