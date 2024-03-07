4cc64d12951c33c8ec63851af3145729d2252608
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-07 07:18:40 +0530

    add: my properties page

---
+++ src/app/properties/page.tsx
@@ -0,0 +1,19 @@
+import { EmptyState } from "@/components";
+import getUser from "@/actions/getUser";
+import getListings from "@/actions/getListings";
+import PropertiesClient from "./PropertiesClient";
+
+export default async function Properties() {
+  const user = await getUser();
+  const listings = await getListings({ userId: user?.id });
+
+  if (!user) {
+    return <EmptyState title="Unauthorized!" subtitle="Please login" />;
+  }
+
+  if (listings.length === 0) {
+    return <EmptyState title="No properties found!" subtitle="Looks like you haven't listed any properties" />;
+  }
+
+  return <PropertiesClient listings={listings} user={user} />;
+}
