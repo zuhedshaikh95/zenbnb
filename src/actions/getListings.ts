7306a0dd364bc8da55797200686d9b4e1e463d40
zuhedshaikh95 <zuhed95@gmail.com>
2024-03-07 07:16:22 +0530

    add: getListings by userId

--- src/actions/getListings.ts
+++ src/actions/getListings.ts
@@ -1,8 +1,17 @@
 import { prisma } from "@/libs";
 
-export default async function getListings() {
+interface ParamsI {
+  userId?: string;
+}
+
+export default async function getListings(params?: ParamsI) {
+  const query = {
+    ...(params?.userId && { userId: params.userId }),
+  };
+
   try {
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: "desc",
       },
